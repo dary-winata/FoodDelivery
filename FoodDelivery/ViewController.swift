0f7357acab98ccdba89f476bcd5ae085ecb3cf05
dary-winata <dary.winata15@gmail.com>
2024-06-27 01:28:59 +0700

    Initial Commit

---
+++ FoodDelivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDelivery
+//
+//  Created by dary winata nugraha djati on 27/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
